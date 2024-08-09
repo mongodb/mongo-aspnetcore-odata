57e849952f2caff56ab830877405e19a6f14d6db
Jania Vandevoorde <98466424+janiavdv@users.noreply.github.com>
2024-08-09 15:49:14 -0400

    ODATA-29: Implement execution of calculations on database level  (#17)
    * Create compute test
    
    * Investigate possibility to rewrite Select
    
    Work in progress. EntitySet<Compute|OrderBy|SelectExpand>Tests all fail. Commit made to show current status of investigation.
    
    * Call Select in new Expression instead of SelectSome
    
    * Hardcode Expression.MemberInit() Type
    
    * Correct selectMethod to System.Linq.IQueryable
    
    * Change double to be not-nullable
    
    * Tidy up
    
    * Refactor VisitSelectSome()
    
    Create helper methods and change hard code to for loop.
    
    * Rewrite Select to new BsonDocument
    
    Implementation incomplete. Commiting to save progress.
    
    * Remove unused methods/class
    
    * Change ArrayInit to ListInit
    
    * Change Add method to {string, BsonValue}
    
    * Add more tests for compute + select
    
    * Begin query formatting
    
    * Format queryable with SelectSome for successful OData request
    
    * Refactor
    
    * Refactor: move RemoveQuotes() to utils class
    
    * Refactor: remove _nameSet
    
    * Remove call to projection method
    
    * Refactor
    
    * Simplify MQL by removing checks for null root
    
    * Add select all tests

---
+++ tests/MongoDB.AspNetCore.OData.Tests/EntitySetComputeTests.cs
@@ -0,0 +1,41 @@
+// Copyright 2023-present MongoDB Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License")
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MongoDB.AspNetCore.OData.Tests;
+
+[TestClass]
+public class EntitySetComputeTests
+{
+    [TestMethod]
+    [DataRow("/odata/Cities?$compute=Population div Density as Area&$select=Id,Area",
+        "cities_compute(Area)select(Id,Area)", DisplayName = "compute_area_select2")]
+    [DataRow("/odata/Cities?$compute=Population div Density as Area&$select=Id,Name,Area",
+        "cities_compute(Area)select(Id,Name,Area)", DisplayName = "compute_area_select3")]
+    [DataRow("/odata/Cities?$compute=Population div Density as Area,Population mul Density as PopulationDensity&$select=Id,Name,Area,PopulationDensity",
+        "cities_compute(Area,PopulationDensity)select(Id,Name,Area,PopulationDensity)", DisplayName = "compute_twice_select4")]
+    [DataRow("/odata/Cities?$compute=Population div Density as Area&$compute=(Population div Density) mul Density as Population2&$select=Id,Area,Population,Population2",
+        "cities_compute(Area,Population2)select(Id,Area,Population,Population2)", DisplayName = "compute_nested_select4")]
+    [DataRow("/odata/Cities?$compute=Population div Density as Area&$select=*",
+        "cities_compute(Area)select(*)", DisplayName = "compute_area_select*")]
+    [DataRow("/odata/Cities?$compute=Population div Density as Area",
+        "cities_compute(Area)", DisplayName = "compute_area")]
+    [DataRow("/odata/Cities?$compute=Population div Density as Area&$select=Id",
+        "cities_compute(Area)select(Id)", DisplayName = "compute_area_select1")]
+    public Task ComputeAsync(string requestUrl, string schemaName)
+        => TestServer.GetAndValidateODataRequestAsync(requestUrl, schemaName);
+
+}
